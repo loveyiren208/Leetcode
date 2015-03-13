c0a95e3277008200ee7f71af2dc0b06d4110837b
Xiaonan Wang <Vivian@Xiaonans-iMac.users.condointernet.net>
2015-03-12 21:35:32 -0700

    Two sum and find median

---
+++ src/com/chanelnan/Median/Median.java
@@ -0,0 +1,80 @@
+package com.chanelnan.Median;
+
+import java.util.Arrays;
+
+/**
+ * Created by Vivian on 3/12/15.
+ */
+public class Median {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int length1 = A.length;
+        int length2 = B.length;
+        if (length1 == 0) {
+            if ( length2 % 2 != 0 ) {
+                return (double)B[ (length2 + 1) / 2 - 1];
+            } else {
+                return (double)(B[length2/2 -1] + B[length2/2]) / 2;
+            }
+        }
+
+        if (length2 == 0) {
+            if ( length1 % 2 != 0 ) {
+                return (double)A[ (length1 + 1) / 2 - 1];
+            } else {
+                return (double)(A[length1/2 -1] + A[length1/2]) / 2;
+            }
+        }
+
+        if ((length1 + length2) % 2 != 0) {
+            return (double)findKth(A, 0, length1 - 1, B, 0, length2 - 1, (length1 + length2 + 1) / 2);
+        } else {
+            return (double) (findKth(A, 0, length1 - 1, B, 0, length2 -1, (length1 + length2) / 2 )
+                    + findKth(A, 0, length1 - 1, B, 0, length2 - 1, (length1 + length2) / 2 + 1) ) / 2;
+        }
+    }
+
+    public int findKth(int A[], int a1, int a2, int B[], int b1, int b2, int k) {
+        if ( (a2 - a1) > (b2 - b1) ) {
+            return findKth(B, b1, b2, A, a1, a2, k);
+        }
+
+        if (b1 == b2) {
+            if (k == 1) {
+                return A[a1] > B[b1] ? B[b1] : A[a1];
+            } else {
+                return A[a1] >B[b1] ? A[a1] : B[b1];
+            }
+        }
+
+        if (a2 == a1) {
+            if ( B[b1 + k - 2] >= A[a1]) {
+                return B[b1 + k - 2];
+            } else {
+                return A[a1] > B[b1 + k -1] ? B[b1 + k -1 ] : A[a1];
+            }
+        }
+
+        int indexGap = (k+1)/2 - 1;
+
+        if ( a2 <= a1 + indexGap) {
+            int index2 = b1 + indexGap + a1 + indexGap - a2;
+            if (A[a2] > B[index2]) {
+                return findKth(A, a1, a2, B, index2, b2, k - (index2 - b1));
+            } else if (A[a2] == B[index2]) {
+                return A[a2];
+            } else {
+                return findKth(A, a2, a2, B, b1, index2, k - (a2 - a1));
+            }
+        }
+
+        if (A[a1 + indexGap] == B[b1 + indexGap]) {
+            return A[a1 + indexGap];
+        } else if (A[a1 + indexGap] > B[b1 + indexGap]) {
+            return findKth(A, a1, a1 + indexGap, B, b1 + indexGap, b2, k - indexGap );
+        } else {
+            return findKth(A, a1 + indexGap, a2, B, b1, b1 + indexGap, k - indexGap);
+        }
+
+        "".substring()
+    }
+}
