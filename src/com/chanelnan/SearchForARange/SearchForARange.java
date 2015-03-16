3adc8186db3af6f396d35add061d0c136b7a0b2c
chanelnan <vivianwong0403@gmail.com>
2015-03-15 23:39:41 -0700

    Binary Search! Search for a range

---
+++ src/com/chanelnan/SearchForARange/SearchForARange.java
@@ -0,0 +1,50 @@
+package com.chanelnan.SearchForARange;
+
+/**
+ * Search for a Range
+ * https://leetcode.com/problems/search-for-a-range/
+ *
+ * Given a sorted array of integers, find the starting and ending position of a given target value.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * If the target is not found in the array, return [-1, -1].
+ * For example,
+ * Given [5, 7, 7, 8, 8, 10] and target value 8,
+ * return [3, 4].
+ *
+ * Created by Vivian on 3/15/15.
+ */
+public class SearchForARange {
+    public int[] searchRange(int[] A, int target) {
+        int[] result = searchHelper(A, 0, A.length, target);
+        if(result[1] == -1) {
+            return result;
+        } else {
+            result[1] = result[1] - 1;
+            return result;
+        }
+    }
+
+    private int[] searchHelper(int[] A, int start, int end, int target){
+        if (end - start == 1) {
+            if (A[start] == target) {
+                return new int[]{start, end};
+            } else {
+                return new int[]{-1, -1};
+            }
+        }
+        if (A[start] > target || A[end - 1] < target) {
+            return new int[]{-1, -1};
+        }
+        int middle = (end + start)/2;
+        int[] first = searchHelper(A, start, middle, target);
+        int[] second = searchHelper(A, middle, end, target);
+        if (first[1] == -1) {
+            return second;
+        } else if (second[1] == -1) {
+            return first;
+        } else {
+            return new int[]{first[0], second[1]};
+        }
+
+    }
+}
